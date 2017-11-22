ac7cb4a5f3dea1bb09bd167659e5991bf816d25a
anyulong <1339918614@qq.com>
2017-11-22 19:54:07 +0800

    增加显示人数     简化代码

--- src/main/java/com/example/StudentManager.java
+++ src/main/java/com/example/StudentManager.java
@@ -17,14 +17,42 @@ public class StudentManager {
 		}
 		
 		//列出不同成绩学生list
-		System.out.println(checkStudent(students, s->s.getScore()==85));
-		System.out.println(checkStudent(students, s->s.getScore()==90));
-		System.out.println(checkStudent(students, s->s.getScore()==95));
-		System.out.println(checkStudent(students, s->s.getScore()==100));
+		
+//		System.out.println("成绩为85的学生有"+checkStudent(students, s->s.getScore()==85).size()+"人。分别是：");
+//		List<Student> score85 = checkStudent(students, s->s.getScore()==85);
+//		System.out.println(score85);
+//		System.out.println("成绩为90的学生有"+checkStudent(students, s->s.getScore()==90).size()+"人。分别是：");
+//		List<Student> score90 = checkStudent(students, s->s.getScore()==90);
+//		System.out.println(score90);
+//		System.out.println("成绩为95的学生有"+checkStudent(students, s->s.getScore()==95).size()+"人。分别是：");
+//		List<Student> score95 = checkStudent(students, s->s.getScore()==95);
+//		System.out.println(score95);
+//		System.out.println("成绩为100的学生有"+checkStudent(students, s->s.getScore()==100).size()+"人。分别是：");
+//		List<Student> score100 = checkStudent(students, s->s.getScore()==100);
+//		System.out.println(score100);
+		
+//		printStudent(85,students);
+//		printStudent(90,students);
+//		printStudent(95,students);
+//		printStudent(100,students);
+		
+		for(int score = 85;score<=100;) {
+			printStudent(score,students);
+			score=score+5;
+		}
 		
 	}
 	public static <T> List<T> checkStudent(List<T> students, Predicate<T> pre){
 		
 		return students.stream().filter(pre).collect(Collectors.toList());
+	}
+	//打印学生
+	public static List<Student> printStudent(int score,List<Student> students) {
+		
+		List<Student> scoreStudent = checkStudent(students, s->s.getScore()==85);
+		System.out.println("成绩为"+score+"的学生有"+checkStudent(students, s->s.getScore()==score).size()+"人。分别是：");
+		System.out.println(scoreStudent);
+		return scoreStudent;
+		
 	}
 }
